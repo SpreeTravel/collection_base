896c2482a9b1daf471e73b9ccd3d0ccb12337740
asnioby <asnioby@gmail.com>
2015-05-07 14:07:19 -0400

    Add register option

--- lib/collection_base.rb
+++ lib/collection_base.rb
@@ -3,4 +3,11 @@ require "collection_base/version"
 module CollectionBase
   require 'collection_base/base'
   include Cenithub::Client
+
+  @register = []
+
+  def register (collection)
+    @register << collection
+  end
+
 end
